f8228e6e335b7c60d4cb20672833a86928965506
uanankoi <64210229+uanankoi@users.noreply.github.com>
2020-09-22 10:08:00 +0800

    Updates

---
+++ precache-manifest.cbfc64b1a0c675a85a8bb82b9d171f53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37415c2681bd629a1e351d0ba5e2753",
+    "url": "/helloapp/index.html"
+  },
+  {
+    "revision": "17870c3335ff51f1a504",
+    "url": "/helloapp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "64a6b63e713d47ed482e",
+    "url": "/helloapp/static/js/2.6a70fac3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/helloapp/static/js/2.6a70fac3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17870c3335ff51f1a504",
+    "url": "/helloapp/static/js/main.0685263e.chunk.js"
+  },
+  {
+    "revision": "7274b640d517da065676",
+    "url": "/helloapp/static/js/runtime-main.9cf685fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/helloapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
